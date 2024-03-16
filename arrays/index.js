73c78e4aad42fcf4ef7c3cfc0204a10592d3e043
dhaval-codes <nimisha25dhaval@gmail.com>
2024-03-17 01:47:36 +0530

    learned almost 3 ways to define types of items in an array based on usecases and different practices

---
+++ arrays/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+// const heroPower: number[] = []
+var heroPower = [];
+var allUsers = [];
+allUsers.push({ name: "DHAVAL", isActive: false });
+// suppose we want arrays inside array and so, usually for ML models, we can define them as-
+var mlModels = [
+    [255, 212, 123], [34, 76, 129] // rgb values of pixels of a pic for ex. 
+];
+superHeros.push("spiderman");
+heroPower.push(3);
---
+++ arrays/index.ts
@@ -0,0 +1,22 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string,
+    isActive:boolean
+}
+const allUsers: User[] = []
+
+allUsers.push({name:"DHAVAL", isActive: false})
+
+// suppose we want arrays inside array and so, usually for ML models, we can define them as-
+
+const mlModels: number[][] = [
+    [255,212,123],[34,76,129]// rgb values of pixels of a pic for ex. 
+]
+
+superHeros.push("spiderman")
+heroPower.push(3)
+
+export {} 
