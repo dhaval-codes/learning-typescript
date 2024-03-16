7fbb93985f20a0a3a4cc410300b87b2162e35d05
dhaval-codes <nimisha25dhaval@gmail.com>
2024-03-17 03:05:35 +0530

    understood tuples, and now array methods have been fixed by microsoft for tuples

---
+++ tuples/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = [1, "djp"];
+var user2;
+user2 = ["djp", 19, true];
+var rgb = [255, 255, 255];
+var newUser = [19, "dhaval@gmail.in"];
---
+++ tuples/index.ts
@@ -0,0 +1,12 @@
+const user:(string|number)[] = [1,"djp"]
+let user2:[string,number,boolean]
+
+user2 = ["djp", 19, true]
+
+let rgb: [number,number,number] = [255,255,255]
+
+type User = [number,string]
+
+const newUser:User = [19,"dhaval@gmail.in"]
+
+export {}
