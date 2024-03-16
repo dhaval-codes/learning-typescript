abf08475fa9ed661860b9101859e8ccf2177ae3d
dhaval-codes <nimisha25dhaval@gmail.com>
2024-03-17 03:20:58 +0530

    studied enums and how putting const reduces the js code produced drastically

---
+++ enums/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "Aisle";
+    SeatChoice["MIDDLE"] = "middle";
+    SeatChoice["WINDOW"] = "window";
+    SeatChoice[SeatChoice["FOURTH"] = 0] = "FOURTH";
+})(SeatChoice || (SeatChoice = {}));
+var djpSeat = SeatChoice.AISLE;
---
+++ enums/index.ts
@@ -0,0 +1,12 @@
+enum SeatChoice {
+    AISLE = "Aisle",
+    MIDDLE = "middle",
+    WINDOW = "window",
+    FOURTH = 0
+}
+
+const djpSeat = SeatChoice.AISLE
+
+// use const before enaum reduces js code into less lines
+
+export {}
